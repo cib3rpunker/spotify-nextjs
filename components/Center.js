cf2a1b068bf35528e34ce6b3ba999f87e3e3ffd3
Viktor Óolen <ciberpunk3r@gmail.com>
2021-12-23 15:32:18 -0600

    :construction:WIP | 03:00:11 | 24. Building the Song Component. ▶️Moving the Logout functionality to the Center Component

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@heroicons/react/outline';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { shuffle } from 'lodash';
 import { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -61,9 +61,15 @@ function Center() {
   //console.log(time, "🎼 ~ file: Center.jsx ~ line 50 ~ Center ~ playlist:", playlist)
 
   return (
-    <div className="flex-grow ">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8 ">
-        <div className="flex item-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+        <div
+          className="flex item-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white"
+          onClick={signOut}
+          // onClick={() => {
+          //   signOut();
+          // }}
+        >
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -35,7 +35,7 @@ function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
+        {/* <button className="flex items-center space-x-2 hover:text-white">
           <LoginIcon
             className="h-5 w-5"
             onClick={() => {
@@ -43,7 +43,7 @@ function Sidebar() {
             }}
           />
           <p>Logout</p>
-        </button>
+        </button> */}
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
