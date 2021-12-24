e90d60e0462bdd0f7319d23eed213edaaa83eeda
Viktor Óolen <ciberpunk3r@gmail.com>
2021-12-24 17:52:42 -0600

    BEGIN 03:20:32 | END 03:25:07 | 🚧27. Building the Left Side of the Player Component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -33,7 +33,7 @@ function Sidebar() {
   console.log( time, '🟦 ~ file: Sidebar.js ~ line 30 ~ Sidebar ~ status: ', status );
 
   return (
-    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:-w-[15rem] hidden md:inline-flex">
+    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:-w-[15rem] hidden md:inline-flex pb-36 ">
       <div className="space-y-4">
         {/* <button className="flex items-center space-x-2 hover:text-white">
           <LoginIcon
