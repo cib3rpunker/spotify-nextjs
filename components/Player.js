280f4e886e6382e0080b1698a49562c7a310d882
ciberpunk3r <ciberpunk3r@gmail.com>
2021-12-26 00:00:15 -0600

    ⏲️03:39:17 | 03:47:18 :sparkles: 31. Implementing Volume Functionality using Debounce

--- components/Player.js
+++ components/Player.js
@@ -13,8 +13,10 @@ import {
   SwitchHorizontalIcon,
 } from '@heroicons/react/solid';
 
+import { debounce } from 'lodash';
+
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSongInfo from '../hooks/useSongInfo';
@@ -63,6 +65,19 @@ function Player() {
     }
   }, [currentTrackIdState, spotifyApi, session]);
 
+useEffect(() => {
+  if (volume > 0 && volume < 100) {
+    debouncedAdjustVolume(volume);
+  }
+}, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch(err => {})
+    }, 300),
+    []
+  );
+
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left  */}
@@ -102,16 +117,22 @@ function Player() {
 
       {/* Right  */}
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeDownIcon className="button" />
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
         <input
           className="w-14 md:w-28"
           type="range"
           value={volume}
-          onChange={e => setVolume(Number(e.target.value))}
+          onChange={(e) => setVolume(Number(e.target.value))}
           min={0}
           max={100}
         />
-        <VolumeUpIcon className="button" />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
